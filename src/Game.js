044ab7fe2395c1396c56c0205ffc914ec58e62fa
RahmaIday <rahma.idayath@outlook.com>
2025-03-06 17:51:22 +1100

    navbar added to game

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Game.css';
+import soccerball from './images/soccerball.svg';
 
 function Game() {
   return (
     <div>
-      <div className='NavBar'></div>
+      <div className='NavBar'>
+        <div className='nav-title'> Soccer Game</div>
+        <div className='scoreboard'>
+          <img className='soccer-logo' src={soccerball} />
+          <div className='score'> 0 </div>
+        </div>
+      </div>
       <div className="soccer-background">
       </div>
     </div>
